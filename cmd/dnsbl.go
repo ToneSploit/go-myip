17093c43487d0410db043285d23e8c857c4716a6
Sebastian Broekhoven <s.broekhoven@gmail.com>
2020-01-07 23:38:05 +0100

    ok

--- cmd/dnsbl.go
+++ cmd/dnsbl.go
@@ -1,62 +1,87 @@
-package dnsbl
+package main
 
 import (
-	"strings"
+	"bufio"
+	"os"
 
-	"github.com/miekg/dns"
+	"github.com/mattn/go-colorable"
+	"github.com/sirupsen/logrus"
+	"github.com/binaryfigments/dnsbl"
 )
 
-func Check(ip string, blacklist string, nameserver string) ([]string, error) {
-
-	rev, err := reverseIPv4(ip)
-	if err != nil {
-		return nil, err
-	}
-
-	answer, err := getA(rev+"."+blacklist, nameserver)
-	if err != nil {
-		return nil, err
-	}
-
-	return answer, nil
-}
-
-func getA(hostname string, nameserver string) ([]string, error) {
-	var record []string
-	m := new(dns.Msg)
-	m.SetQuestion(dns.Fqdn(hostname), dns.TypeA)
-	c := new(dns.Client)
-	m.MsgHdr.RecursionDesired = true
-	in, _, err := c.Exchange(m, nameserver+":53")
-	if err != nil {
-		return nil, err
-	}
-	for _, rin := range in.Answer {
-		if r, ok := rin.(*dns.A); ok {
-			record = append(record, r.A.String())
-		}
-	}
-	return record, nil
-}
-
-func reverseIPv4(ip string) (string, error) {
-	PTR, err := dns.ReverseAddr(ip)
-	if err != nil {
-		return "", err
-	}
-
-	reversed := strings.TrimSuffix(PTR, ".in-addr.arpa.")
-
-	return reversed, nil
-}
-
 /*
+	http://www.sorbs.net/using.shtml
+
+	http.dnsbl.sorbs.net    127.0.0.2
+	socks.dnsbl.sorbs.net    127.0.0.3
+	misc.dnsbl.sorbs.net    127.0.0.4
+	smtp.dnsbl.sorbs.net    127.0.0.5
+	new.spam.dnsbl.sorbs.net    127.0.0.6
+	recent.spam.dnsbl.sorbs.net    127.0.0.6
+	old.spam.dnsbl.sorbs.net    127.0.0.6
+	spam.dnsbl.sorbs.net    127.0.0.6
+	escalations.dnsbl.sorbs.net    127.0.0.6
+	web.dnsbl.sorbs.net    127.0.0.7
+	block.dnsbl.sorbs.net    127.0.0.8
+	zombie.dnsbl.sorbs.net    127.0.0.9
+	dul.dnsbl.sorbs.net    127.0.0.10
+	badconf.rhsbl.sorbs.net    127.0.0.11
+	nomail.rhsbl.sorbs.net    127.0.0.12
+	noserver.dnsbl.sorbs.net    127.0.0.14
+	virus.dnsbl.sorbs.net    127.0.0.15
+*/
+
+func main() {
+	logrus.SetFormatter(&logrus.TextFormatter{ForceColors: true})
+	logrus.SetOutput(colorable.NewColorableStdout())
+
+	// logrus.Info("succeeded")
+	// logrus.Warn("not correct")
+	// logrus.Error("something error")
+	// logrus.Fatal("panic")
+
+	file, err := os.Open("ips.txt")
+	if err != nil {
+		logrus.Fatal(err.Error())
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// fmt.Println(scanner.Text())
+
+		for _, bl := range Blacklists {
+			response, err := dnsbl.Check(scanner.Text(), bl, "8.8.8.8")
+
+			if err != nil {
+				logrus.WithFields(logrus.Fields{
+					"IP":       scanner.Text(),
+					"BL":       bl,
+					"Response": err.Error(),
+				}).Error("done")
+				continue
+			}
+			if len(response) < 0 {
+				logrus.WithFields(logrus.Fields{
+					"IP":       scanner.Text(),
+					"BL":       bl,
+					"Response": response,
+				}).Info("done")
+			}
+		}
+		// logrus.Warn(len(response))
+	}
+
+	if err := scanner.Err(); err != nil {
+		logrus.Fatal(err.Error())
+	}
+}
+
+
 var Blacklists = []string{
 	"aspews.ext.sorbs.net",
 	"b.barracudacentral.org",
 	"bl.deadbeef.com",
-	"bl.emailbasura.org",
-	"bl.spamcannibal.org",
 	"bl.spamcop.net",
 	"blackholes.five-ten-sg.com",
 	"blacklist.woody.ch",
@@ -130,5 +155,3 @@ var Blacklists = []string{
 	"xbl.spamhaus.org",
 	"zen.spamhaus.org",
 	"zombie.dnsbl.sorbs.net"}
-
-*/
